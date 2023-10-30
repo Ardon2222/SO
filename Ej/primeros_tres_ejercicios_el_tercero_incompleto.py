bdcace7a5a6fd64472923e42fc21df1bcec6a15d
Ardon2222 <elencheve699@gmail.com>
2023-10-30 15:56:10 +0100

    dos

---
+++ Ej/primeros_tres_ejercicios_el_tercero_incompleto.py
@@ -0,0 +1,35 @@
+#def metodo1(a, b):
+#   return ((a + b * b - c)/5*b)
+
+#def metodo2(array):
+#    print (array)
+#   for n in array:
+#       print("Mi nombre es: ", n)
+
+def metodo3():
+   while True:
+
+       time.sleep(1)
+       print(Mimiendo estoy)
+
+
+#if __name__ == "__main__":
+
+#   METODO 1 (SUMAS):
+
+#c = 1
+#res = metodo1(2, 3)
+#metodo1(2,3)
+#print (res)
+#--------------------------
+
+#   METODO 2 (Array):
+#array = ['ar1', 'ar2', 'ar3']
+#metodo2(array)
+#--------------------------
+
+#   METODO 3 (Bulce infinito):
+buc = metodo3()
+metodo3()
+print (buc)
+#--------------------------
